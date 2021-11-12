a342af5c4599abfe7c1a79ad6ed97cada9ae4935
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2021-11-12 08:15:23 +0100

    Created 3 widgets which DateBlock for history on a certain date which contains PastPurchases block which also contains different lists of items purchased with its data

--- lib/Widgets/DateBlock.dart
+++ lib/Widgets/DateBlock.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myfoodz/Widgets/PastPurchases.dart';
 
 class DateBlock extends StatefulWidget {
   const DateBlock({ Key? key }) : super(key: key);
@@ -10,10 +11,15 @@ class DateBlock extends StatefulWidget {
 class _DateBlockState extends State<DateBlock> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('Last Week on Monday',style: TextStyle(fontFamily: 'Poppins'),)
-      ],
+    return Container(
+      width: 280,
+      child: Column(
+        children: [
+          Text('Last Week on Monday',style: TextStyle(fontFamily: 'Poppins', fontSize: 18),),
+          SizedBox(height: 20,),
+          PastPurchases()
+        ],
+      ),
     );
   }
 }
