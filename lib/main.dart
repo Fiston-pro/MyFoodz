9238357b2db753e79c602bb7e2f84e4976966618
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2022-02-27 22:33:45 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 //Screens
@@ -7,6 +6,7 @@ import 'package:myfoodz/Screens/Chart.dart';
 import 'package:myfoodz/Screens/Home.dart';
 import 'package:myfoodz/Screens/SplashScreen.dart';
 //firebase
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myfoodz/Screens/signin.dart';
 import 'package:myfoodz/modal.dart';
