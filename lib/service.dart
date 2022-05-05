f8633cce1447954125a526af1f8dd983c4106051
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2022-05-05 23:59:32 +0200

    I deleted unecessary imports in service.dart file

--- lib/service.dart
+++ lib/service.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter_login/flutter_login.dart'; //Login page package
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myfoodz/modal.dart';
 import 'package:myfoodz/providers.dart';// modal for user data
 
 //Firestore database class
